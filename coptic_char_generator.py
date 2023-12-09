fixed vs random masking toggle

coptic_char_generator.py
@@ -61,8 +61,8 @@ def check_accuracy(target, orig_data_item):
         if orig_data_item.labels[j] > 0:
             # masked token
             masked += 1
-            logger.info(f"actual labels: {orig_data_item.labels[j]}")
-            logger.info(f"prediction: {target[j]}")
+            logger.debug(f"actual labels: {orig_data_item.labels[j]}")
+            logger.debug(f"prediction: {target[j]}")
             if target[j] == orig_data_item.labels[j]:
                 # prediction is correct
                 correct += 1
@@ -70,7 +70,9 @@ def check_accuracy(target, orig_data_item):
     return masked, correct
 
 
-def train_batch(model, optimizer, criterion, data, data_indexes, update=True):
+def train_batch(
+    model, optimizer, criterion, data, data_indexes, update=True, mask=True
+):
     model.zero_grad()
     total_loss, total_tokens, total_chars = 0, 0, 0
 
@@ -82,8 +84,9 @@ def train_batch(model, optimizer, criterion, data, data_indexes, update=True):
     for i in data_indexes:
         data_item = data[i]
 
-        # data_item, mask_count = model.mask_and_label_characters(data_item)
-        # total_masked += mask_count
+        if mask:
+            data_item, mask_count = model.mask_and_label_characters(data_item)
+            total_masked += mask_count
 
         index_tensor = torch.tensor(data_item.indexes, dtype=torch.int64).to(device)
         label_tensor = torch.tensor(data_item.labels, dtype=torch.int64).to(device)
@@ -124,7 +127,9 @@ def train_batch(model, optimizer, criterion, data, data_indexes, update=True):
     return total_loss, total_tokens, total_chars, total_masked, dev_masked, dev_correct
 
 
-def train_model(model, train_data, dev_data=None, output_name="charLM"):
+def train_model(
+    model, train_data, dev_data=None, output_name="coptic_lacuna", mask=True
+):
     data_list = [i for i in range(len(train_data))]
 
     if dev_data is None:
@@ -163,6 +168,7 @@ def train_model(model, train_data, dev_data=None, output_name="charLM"):
                 train_data,
                 train_list[i : i + incremental_batch_size],
                 update=True,
+                mask=True,
             )
             train_loss += loss
             train_tokens += num_tokens
@@ -184,17 +190,14 @@ def train_model(model, train_data, dev_data=None, output_name="charLM"):
             dev_masked,
             dev_correct,
         ) = train_batch(
-            model,
-            optimizer,
-            criterion,
-            dev_data,
-            dev_list,
-            update=False,  # ???
+            model, optimizer, criterion, dev_data, dev_list, update=False, mask=True
         )
 
         if epoch == 0:
             logger.info(
-                f"train={len(train_list):,} {train_tokens:,} {train_chars:,} {train_chars / train_tokens:0.1f} dev={len(dev_list):,} {dev_tokens:,} {dev_chars:,} {dev_chars / dev_tokens:0.1f} bs={batch_size} lr={learning_rate} {model.specs}"
+                f"train={len(train_list):,} {train_tokens:,} {train_chars:,} {train_chars / train_tokens:0.1f} "
+                f"dev={len(dev_list):,} {dev_tokens:,} {dev_chars:,} {dev_chars / dev_tokens:0.1f} "
+                f"bs={batch_size} lr={learning_rate} {model.specs}"
             )
 
         logging.info(time.time())
@@ -312,12 +315,12 @@ def accuracy_evaluation(model, data, data_indexes):
             best = best.data.item()
             target.append(best)
 
-        logger.info("In accuracy")
+        logger.debug("In accuracy")
         masked, correct_guess = check_accuracy(target, data_item)
         masked_total += masked
         correct += correct_guess
 
-    if masked_total >= 0:
+    if masked_total > 0:
         logging.info(
             f"dev masked total: {masked_total}, correct predictions: {correct}, simple accuracy: {round(correct/masked_total, 3)}"
         )